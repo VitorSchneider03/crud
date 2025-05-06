8062020eb7477315ba8a89626c8189e00c2f508f
VitorSchneider03 <flotita.editor@gmail.com>
2025-05-06 17:26:33 -0300

    configured user avatar display

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,22 @@
+import 'package:crud_flutter/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Joe',
+    email: 'joe123@joe.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2025/04/26/16/35/ai-generated-9561553_1280.png',
+  ),
+  '2': const User(
+    id: '2',
+    name: 'Maria',
+    email: 'maria@job.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2021/10/08/20/28/hunter-6692462_1280.png',
+  ),
+  '3': const User(
+    id: '3',
+    name: 'Ana',
+    email: 'ana@anagmail.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2023/01/23/23/57/ai-generated-7739914_960_720.jpg',
+  ),
+}; 
